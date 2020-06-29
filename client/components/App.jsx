88cd3b616d60e09cb694aa0309e9edf0994729af
matthew-devonport <roarsintheeast@gmail.com>
2020-06-29 19:45:32 +1200

    basic rendering of logo on page

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,31 +1,21 @@
 import React from 'react'
+import { HashRouter as Router, Route } from 'react-router-dom';
 
-import { getFruits } from '../apiClient'
+import Home from './Home'
 
 class App extends React.Component {
-  state = {
-    fruits: []
-  }
-
-  componentDidMount () {
-    getFruits()
-      .then(fruits => {
-        this.setState({fruits})
-      })
-  }
+ 
 
   render () {
     return (
-      <div className='app'>
-        <h1>Fullstack Boilerplate</h1>
-        <ul>
-          {this.state.fruits.map(fruit => (
-            <li key={fruit}>{fruit}</li>
-          ))}
-        </ul>
-      </div>
+      <Router>
+        <div>
+        <Route exact path='/' component={Home} />
+        </div>
+      </Router>
     )
   }
 }
+
 
 export default App
---
+++ client/components/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+
+class Home extends React.Component {
+
+
+    render() {
+        return (
+            <React.Fragment>
+            <img src="./img/logo.png" alt="Main Logo" className="mainLogo"></img>
+            </React.Fragment>
+
+        )
+    }
+}
+
+
+export default Home
