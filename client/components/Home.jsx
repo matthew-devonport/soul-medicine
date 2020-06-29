064628485ca012b25f6eb43b1c105b17f2e53ad4
matthew-devonport <roarsintheeast@gmail.com>
2020-06-29 20:29:04 +1200

    really basic home page done

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -9,6 +9,13 @@ class Home extends React.Component {
         return (
             <React.Fragment>
             <img src="./img/logo.png" alt="Main Logo" className="mainLogo"></img>
+            <div className="homeLinks">
+             <a className="gradient link">ABOUT</a>
+             <a className="gradient link">GALLERY</a>
+             <a className="gradient link">CONTACT</a>
+             <a className="gradient link">SOUND</a>
+            </div>
+
             </React.Fragment>
 
         )
