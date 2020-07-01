75ff2af69b5535095b31e909141e7216419aee47
matthew-devonport <roarsintheeast@gmail.com>
2020-07-01 16:06:45 +1200

    changed front page styling

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -8,12 +8,18 @@ class Home extends React.Component {
     render() {
         return (
             <React.Fragment>
+                <div className="background">
             <img src="./img/logo.png" alt="Main Logo" className="mainLogo"></img>
+            <div><i className="statement">"Empowering others with healing tools"</i>
+            </div>
+            <div className="width">
             <div className="homeLinks">
              <a className="gradient link">ABOUT</a>
              <a className="gradient link">GALLERY</a>
              <a className="gradient link">CONTACT</a>
-             <a className="gradient link">SOUND</a>
+             <a className="gradient link">MEDITATION</a>
+            </div>
+            </div>
             </div>
             <Footer />
             </React.Fragment>
