83efca6fd9057f372f878e80d70d02866ac31430
matthew-devonport <roarsintheeast@gmail.com>
2020-07-02 11:24:24 +1200

    draft home page done

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -8,11 +8,18 @@ class Home extends React.Component {
     render() {
         return (
             <React.Fragment>
-                <div className="background">
-            <img src="./img/logo.png" alt="Main Logo" className="mainLogo"></img>
-            <div><i className="statement">"Empowering others with healing tools"</i>
+            <div className="background">
+            <div className="contact">
+                <div className="contactArea">
+                <img src="./img/phone.png" alt="Phone Logo" className="phoneLogo"></img>
+                    <a className="contactLink">09 9837 6288 |</a>
+                    <a className="contactLink">021 161 1540</a>
+                </div>
             </div>
-            <div className="width">
+            <div className="mainFlex">
+            <img src="./img/logo.png" alt="Main Logo" className="mainLogo"></img>
+            <div className="flexStatement"><i className="statement">"Empowering others with healing tools"</i>
+            </div>
             <div className="homeLinks">
              <a className="gradient link">ABOUT</a>
              <a className="gradient link">GALLERY</a>
