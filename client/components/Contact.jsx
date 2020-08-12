ec2bf3ea00749b564f0d7a8b1a7a5f22baa92f07
matthew-devonport <roarsintheeast@gmail.com>
2020-08-12 13:06:43 +1200

    basic contact me done

--- client/components/Contact.jsx
+++ client/components/Contact.jsx
@@ -12,7 +12,7 @@ class About extends React.Component {
                 <div className="background">
                     <div className="contactHeader">
                         <div>
-                        <div className="mainFlex">
+                        <div className="contactFlex">
                             <img src="./img/logo.png" alt="Main Logo" className="aboutLogo"></img>
                             <div className="navLinks">
                             <Link to="/" className="gradient navLink">HOME</Link>
@@ -25,13 +25,16 @@ class About extends React.Component {
                             </div>
                         </div>
                     </div>
-                    <div className="aboutBody">
-                        <div className="bodyMargin">
-                            <div className="picWrap">
+                    <div className="contactBody">
+                        <div className="contactInfo">We would love to hear from you!<br /><br /> 
+                        If you have any questions or queries, please click one of the preferred contact methods below!
                         </div>
-                            <div className="bodyText">
-                        </div>
-                        </div>
+                <a href="tel:0211611540" className="contactColumn">
+               <img src="./img/phone.png" alt="Phone Icon" className="phoneLogo"></img>  
+               </a>
+               <a href="mailto:carolmcm@xtra.co.nz"  className="contactColumn">  
+               <img src="./img/email.png" alt="Email Icon" className="emailLogo"></img>
+               </a>
                     </div>
                     <div className="footerFlex">
                         <Footer />
